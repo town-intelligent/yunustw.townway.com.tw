bc9d708fd96b0c0e5fb142d828dc39153bef3a69
HuangJyunYu <yillkid@gmail.com>
2023-05-27 23:41:49 +0800

    Create config.js

---
+++ static/js/config.js
@@ -0,0 +1,9 @@
+// Host
+HOST_URL_EID_DAEMON = "https://beta-eid-backend.townway.com.tw";
+HOST_URL_TPLANET_DAEMON = "https://beta-tplanet-backend.townway.com.tw";
+
+// EID
+HOST_URL_EID = "https://beta-eid.townway.com.tw";
+
+// Site hoster
+SITE_HOSTERS = ["forus999@gmail.com"]
