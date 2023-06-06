e4ab47c6dcdb64ffe3f85fb7a592abb2a12a23fe
HuangJyunYu <yillkid@gmail.com>
2023-06-06 12:04:47 +0800

    Update config.js

--- static/js/config.js
+++ static/js/config.js
@@ -6,5 +6,4 @@ HOST_URL_TPLANET_DAEMON = "https://tplanet-backend.townway.com.tw";
 HOST_URL_EID = "https://eid.townway.com.tw";
 
 // Site hoster
-// "mickeypeng@tpwl.org"
-SITE_HOSTERS = ["forus999@gmail.com", "secondhome2023.1@gmail.com"]
+SITE_HOSTERS = ["forus999@gmail.com", "secondhome2023.1@gmail.com", "mickeypeng@tpwl.org"]
