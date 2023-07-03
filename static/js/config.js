4c41b6308e15e01500fa361b49ba46d3fbe61d54
yillkid <yillkid@gmail.com>
2023-07-03 15:56:51 +0800

    fix: revise config file

--- static/js/config.js
+++ static/js/config.js
@@ -1,9 +1,9 @@
 // Host
-HOST_URL_EID_DAEMON = "https://beta-eid-backend.townway.com.tw";
-HOST_URL_TPLANET_DAEMON = "https://beta-tplanet-backend.townway.com.tw";
+HOST_URL_EID_DAEMON = "https://eid-backend.townway.com.tw";
+HOST_URL_TPLANET_DAEMON = "https://tplanet-backend.townway.com.tw";
 
 // EID
-HOST_URL_EID = "https://beta-eid.townway.com.tw";
+HOST_URL_EID = "https://eid.townway.com.tw";
 
 // Site hoster
-SITE_HOSTERS = ["forus999@gmail.com", "secondhome2023.1@gmail.com", "ysnp-gov@gmail.com", "mickeypeng@tpwl.org"]
+SITE_HOSTERS = ["jinliao0601@gmail.com"]
