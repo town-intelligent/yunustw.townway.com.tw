2bfe829aa8c44145f1ccc1b060227ef50b663943
HuangJyunYu <yillkid@gmail.com>
2023-07-19 21:18:43 +0800

    Update config.js

--- static/js/config.js
+++ static/js/config.js
@@ -1,6 +1,6 @@
 // Host
-HOST_URL_EID_DAEMON = "https://beta-eid-backend.townway.com.tw";
-HOST_URL_TPLANET_DAEMON = "https://beta-tplanet-backend.townway.com.tw";
+HOST_URL_EID_DAEMON = "https://beta2-eid-backend.townway.com.tw";
+HOST_URL_TPLANET_DAEMON = "https://beta2-tplanet-backend.townway.com.tw";
 
 // EID
 HOST_URL_EID = "https://beta-eid.townway.com.tw";
