2aa7aec7eb50e7bee52b5592ce7647e858c8f14a
yillkid <yillkid@gmail.com>
2023-08-07 13:03:48 +0800

    fix: 盤點平台 Oauth

--- static/js/config.js
+++ static/js/config.js
@@ -1,9 +1,9 @@
 // Host
-HOST_URL_EID_DAEMON = "https://eid-backend.townway.com.tw";
-HOST_URL_TPLANET_DAEMON = "https://tplanet-backend.townway.com.tw";
+HOST_URL_EID_DAEMON = "https://beta-eid-backend.townway.com.tw";
+HOST_URL_TPLANET_DAEMON = "https://beta-tplanet-backend.townway.com.tw";
 
 // EID
-HOST_URL_EID = "https://eid.townway.com.tw";
+HOST_URL_EID = "https://alpha-eid.townway.com.tw";
 
 // Site hoster
 SITE_HOSTERS = ["forus999@gmail.com", "secondhome2023.1@gmail.com", "mickeypeng@tpwl.org"]
