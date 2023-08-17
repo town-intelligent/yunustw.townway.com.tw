8dd2335814df2a9146c9179791fe910bc3e2c6a5
yillkid <yillkid@gmail.com>
2023-08-17 20:46:19 +0800

    fix: CNAME & URL

--- static/js/config.js
+++ static/js/config.js
@@ -3,7 +3,7 @@ HOST_URL_EID_DAEMON = "https://eid-backend.townway.com.tw";
 HOST_URL_TPLANET_DAEMON = "https://tplanet-backend.townway.com.tw";
 
 // EID
-HOST_URL_EID = "https://alpha-eid.townway.com.tw";
+HOST_URL_EID = "https://eid.townway.com.tw";
 
 // Site hoster
 SITE_HOSTERS = ["jinliao0601@gmail.com"]
