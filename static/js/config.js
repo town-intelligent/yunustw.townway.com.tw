26af21ad994d6ea1f217a4009d46ba092cb2730d
HuangJyunYu <yillkid@gmail.com>
2023-08-22 22:11:55 +0800

    Update config.js

--- static/js/config.js
+++ static/js/config.js
@@ -9,5 +9,5 @@ HOST_URL_EID = "https://eid.townway.com.tw";
 SITE_HOSTERS = ["ti@4impact.cc", "yunuscentre@yunustw.org"]
 
 // Site information
-SITE_TYPE = 1 // 0 or 1(公板)
+SITE_TYPE = 0 // 0 or 1(公板)
 SITE_NAME = "4impact"
