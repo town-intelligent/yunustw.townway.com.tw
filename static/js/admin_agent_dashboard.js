00b14df3e7220659edccf4bf544ab50c1ebe9fc3
yillkid <yillkid@gmail.com>
2023-08-22 14:35:41 +0800

    fix: specific group on admin agent dashboard

--- static/js/admin_agent_dashboard.js
+++ static/js/admin_agent_dashboard.js
@@ -1,8 +1,8 @@
 export function set_page_info_admin_agent_dashboard() {
   var email = getLocalStorage("email");
-  if (email != SITE_HOSTERS[0]) {
-    document.getElementById("admin_index").style.display = "none";
-    document.getElementById("cms_news_list").style.display = "none";
-    document.getElementById("cms_contact_us").style.display = "none";
+  if (email == SITE_HOSTERS[0]) {
+    document.getElementById("admin_index").style.display = "block";
+    document.getElementById("cms_news_list").style.display = "block";
+    document.getElementById("cms_contact_us").style.display = "block";
   }
 }
