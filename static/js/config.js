b36b466f3c21f3be57025e89331dbef6fb567f34
HuangJyunYu <yillkid@gmail.com>
2023-09-25 12:50:42 +0800

    Update config.js

--- static/js/config.js
+++ static/js/config.js
@@ -1,6 +1,6 @@
 // Host
-HOST_URL_EID_DAEMON = "https://beta-eid-backend.townway.com.tw";
-HOST_URL_TPLANET_DAEMON = "https://beta-tplanet-backend.townway.com.tw";
+HOST_URL_EID_DAEMON = "https://eid-backend.townway.com.tw";
+HOST_URL_TPLANET_DAEMON = "https://tplanet-backend.townway.com.tw";
 
 // EID
 HOST_URL_EID = "https://eid.townway.com.tw";
@@ -10,8 +10,8 @@ SITE_HOSTERS = ["forus999@gmail.com", "secondhome2023.1@gmail.com", "mickeypeng@
 
 // Site information
 SITE_TYPE = 0 // 0 or 1(公板)
-SITE_NAME = "TPLANET"
-COPYRIGHT = "TPLANET"
+SITE_NAME = "Second Home"
+COPYRIGHT = "Second Home"
 
 // Weight : SDGs, 人文地產景, 德智體群美
 WEIGHT = [1, 1, 1]
